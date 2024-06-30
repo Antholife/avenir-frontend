fb310ce25db8c9a24c2ca83d6c41b5b570b54ea4
chahatanthony@gmail.com <antholife@MBP-de-Anthony.lan>
2024-06-30 20:40:50 +0200

    up

--- components/shared/Nav.vue
+++ components/shared/Nav.vue
@@ -9,7 +9,10 @@
           Avenir Immobilier
         </NuxtLink>
       </h3>
-      <div class="panel">
+      <div class="burger" @click="toggleMenu">
+        &#9776;
+      </div>
+      <div :class="['panel', { 'panel-active': isMenuActive }]">
         <NuxtLink class="link" :to="'/news'">Actualités</NuxtLink>
         <NuxtLink class="link" :to="'/sales'">Vente/Achat</NuxtLink>
         <NuxtLink class="link" :to="'/manage'">Location</NuxtLink>
@@ -26,35 +29,21 @@ import sal from 'sal.js'
 sal();
 export default {
   name: "Nav",
+  data() {
+    return {
+      isMenuActive: false
+    };
+  },
+  methods: {
+    toggleMenu() {
+      this.isMenuActive = !this.isMenuActive;
+    }
+  }
 }
 </script>
 
+
 <style scoped>
-
-.panel {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-}
-
-.link {
-  margin: 0 10px;
-  color: white;
-  text-decoration: none;
-  font-weight: 500;
-  cursor: pointer;
-  &:hover {
-    color: #dbae5f;
-  }
-}
-
-.in {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding: 0 20px;
-}
-
 .nav {
   text-align: center;
   z-index: 999;
@@ -64,4 +53,69 @@ export default {
   top: 0;
   left: 0;
 }
+
+.in {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 20px;
+}
+
+.panel {
+  display: flex;
+  justify-content: end;
+  align-items: center;
+  transition: max-height 0.3s ease-out;
+  max-height: 0;
+  overflow: hidden;
+  flex-direction: column;
+  width: 100%;
+}
+
+.panel-active {
+  max-height: 300px; /* Adjust according to the number of items */
+}
+
+.link {
+  margin: 10px 0;
+  color: white;
+  text-decoration: none;
+  font-weight: 500;
+  cursor: pointer;
+  &:hover {
+    color: #dbae5f;
+  }
+}
+
+.burger {
+  display: none;
+  font-size: 30px;
+  cursor: pointer;
+}
+
+@media (min-width: 768px) {
+  .panel {
+    flex-direction: row;
+    max-height: none;
+  }
+
+  .burger {
+    display: none;
+  }
+
+  .link {
+    margin: 0 10px;
+  }
+}
+
+@media (max-width: 767px) {
+  .burger {
+    display: block;
+  }
+
+  .panel {
+    flex-direction: column;
+  }
+}
 </style>
+
